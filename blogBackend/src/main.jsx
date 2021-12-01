cff990c77adad4f8b64428233aa2aa2da04d4a74
dgjungleP <dgJungle_c@163.com>
2021-12-01 16:27:40 +0800

    导航栏

--- blogBackend/src/main.jsx
+++ blogBackend/src/main.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Layout, Menu, Button, Tag } from "antd";
+import { Layout, Menu, Button, Tag, Breadcrumb } from "antd";
 import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -20,20 +20,32 @@ function SiderMenus(props) {
 function HeaderMenus(props) {
   return (
     <>
-      <Header className="site-layout-background" style={{ padding: 0 }}>
-        <Button
-          type="primary"
-          onClick={props.onCollapse}
-          style={{ marginBottom: 16 }}
-        >
-          {React.createElement(
-            props.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined
-          )}
-        </Button>
-        <span style={{ marginLeft: 20 }}>首页</span>
-      </Header>
       <Header
         className="site-layout-background"
+        style={{
+          padding: 0,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Button type="primary" onClick={props.onCollapse}>
+            {React.createElement(
+              props.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined
+            )}
+          </Button>
+          <Breadcrumb style={{ marginLeft: 20, display: "inline-block" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>An Application</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+        <div>
+          <span style={{ paddingRight: 20 }}>Hello</span>
+        </div>
+      </Header>
+      <Header
+        className="site-layout-background title"
         style={{
           height: 40,
           boxShadow: "2px 0px 4px #888888",
@@ -48,10 +60,10 @@ function HeaderMenus(props) {
               key={tag}
               style={{ height: 24 }}
               closable
-              onClose={(tag) => {
-                console.log(tag);
-              }}
+              color={props.selectedTag == tag ? "#87d068" : ""}
+              onClick={() => props.changeSelectedTag(tag)}
             >
+              <span className="point" hidden={props.selectedTag != tag}></span>{" "}
               {tag}
             </Tag>
           );
@@ -62,6 +74,8 @@ function HeaderMenus(props) {
 }
 function BasePage(props) {
   const [collapsed, changeCollapsed] = useState(false);
+  const [selectedTag, changeSelectedTag] = useState("");
+
   const onCollapse = () => {
     changeCollapsed(!collapsed);
   };
@@ -73,7 +87,9 @@ function BasePage(props) {
           <HeaderMenus
             collapsed={collapsed}
             onCollapse={onCollapse}
-            tags={[1, 2, 3]}
+            tags={[111, 2222, 3333]}
+            changeSelectedTag={changeSelectedTag}
+            selectedTag={selectedTag}
           />
           <Content style={{ margin: "0 16px" }}>
             <div
