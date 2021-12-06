e39c366bcb27af23108439bff04887cf3c4f3a10
jungle <dgjungle_c@163.com>
2021-12-06 23:41:54 +0800

    fix

--- blogBackend/src/login.jsx
+++ blogBackend/src/login.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./assest/js/confetti.js";
 function Login(props) {
   const myCanvas = React.createRef();
 
   useEffect(() => {});
   return (
-    <>
+    <div style={{ background: "#000" }}>
       <canvas id="canvas" ref={myCanvas} width="100%" height="100%">
         {" "}
       </canvas>
-
-      <span>hello world</span>
-    </>
+    </div>
   );
 }
 export { Login };
