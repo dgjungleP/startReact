acf16646c1ce1359e1c1d862458f56e7aaf57fa3
jungle <dgjungle_c@163.com>
2022-01-02 00:15:56 +0800

    danshbord

--- blogBackend/src/server/dashbordservice.js
+++ blogBackend/src/server/dashbordservice.js
@@ -3,5 +3,8 @@ const preFix = "/dashboard";
 function getHeader() {
   return axios.get(preFix + "/header");
 }
+function getHeader() {
+  return axios.get(preFix + "/count");
+}
 
 export { getHeader };
