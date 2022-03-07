fe8cd21243084e41b4e15b55c6c3d4a63aafd16d
Jungle <dgJungle_c@163.com>
2022-03-07 21:51:49 +0800

    文件上传

--- clock/src/main.jsx
+++ clock/src/main.jsx
@@ -1,8 +1,101 @@
-import React from "react";
+import { Upload, message, Image, Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { InboxOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css";
+import axios from "axios";
+const { Dragger } = Upload;
+function PictureWall() {
+  const [fileList, setFileList] = useState([]);
+  const flushFileList = () => {
+    axios.get("http://localhost:8080/file/list/image").then((response) => {
+      setFileList(response.data);
+    });
+  };
+  useEffect(() => {
+    flushFileList();
+  }, []);
+  return (
+    <>
+      <Row gutter={[16, 24]} style={{ marginTop: 15, marginBottom: 15 }}>
+        {fileList.map((file) => {
+          return (
+            <Col span={4} key={file.id} style={{}}>
+              <FileCard file={file}></FileCard>
+            </Col>
+          );
+        })}
+      </Row>
 
+      <UploadFile freshList={flushFileList}></UploadFile>
+    </>
+  );
+}
+function UploadFile(props) {
+  const handleDragChange = (info) => {
+    const file = info.file;
+    const { status } = file;
+    if (status !== "uploading") {
+      props.freshList();
+      console.log(status);
+    }
+    if (status === "done") {
+      message.success(`${file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${file.name} file upload failed.`);
+    }
+  };
+  const handlePreview = async (file) => {
+    setPreview(file);
+  };
+  const handleDrop = (e) => {
+    console.log("Dropped files", e.dataTransfer.files);
+  };
+  return (
+    <>
+      <Row justify={"center"}>
+        <Col>
+          <Dragger
+            multiple={true}
+            name="file"
+            onChange={handleDragChange}
+            onDrop={handleDrop}
+            onPreview={handlePreview}
+            action="http://127.0.0.1:8080/file/upload"
+          >
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload. Strictly prohibit from
+              uploading company data or other band files
+            </p>
+          </Dragger>
+        </Col>
+      </Row>
+    </>
+  );
+}
+function FileCard(props) {
+  const file = props.file;
+  return (
+    <>
+      <Image
+        width={200}
+        src={`http://localhost:8080/file/image/${file.id}`}
+      ></Image>
+    </>
+  );
+}
 function Hello() {
-  return <>Hello</>;
+  return (
+    <>
+      <PictureWall></PictureWall>
+    </>
+  );
 }
 ReactDOM.render(
   <React.StrictMode>
