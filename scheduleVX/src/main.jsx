96fc82727bd7eed265f29cd6534dbd315aec1246
dgjungleP <dgJungle_c@163.com>
2022-04-23 08:56:43 +0800

    fix

--- scheduleVX/src/main.jsx
+++ scheduleVX/src/main.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "antd/dist/antd.css";
-import { Card, Col, List, Row, Statistic } from "antd";
+import { Card, Col, List, PageHeader, Row, Statistic } from "antd";
 import { getManagerInfo } from "./service/manager";
 function App(props) {
+  const [managerInfo, setManagerInfo] = useState({});
+
   useEffect(() => {
     var timerID = setInterval(() => {
-      getManagerInfo().then((res) => {
-        console.log(res);
-      });
+      getManagerInfo()
+        .then((res) => {
+          setManagerInfo(res.data);
+          console.log(res);
+        })
+        .catch((res) => {
+          clearInterval(timerID);
+        });
     }, 1000);
     return () => {
       clearInterval(timerID);
@@ -18,8 +25,19 @@ function App(props) {
     <>
       <div>
         <Card title="定时任务看板" type="inner">
-          <ScheduleStatistic />
-          <ScheduleList />
+          <ScheduleStatistic
+            initCount={managerInfo.currentCount}
+            runningCount={managerInfo.runningCount}
+            totalRuningCount={managerInfo.totalRunningTime}
+          />
+          <ScheduleList
+            title="定时任务"
+            data={managerInfo.periodicScheduleList}
+          />
+          <ScheduleList
+            title="单次执行任务"
+            data={managerInfo.timerScheduleList}
+          />
         </Card>
       </div>
     </>
@@ -27,31 +45,20 @@ function App(props) {
 }
 
 function ScheduleList(props) {
-  const data = [
-    {
-      title: "Ant Design Title 1",
-    },
-    {
-      title: "Ant Design Title 2",
-    },
-    {
-      title: "Ant Design Title 3",
-    },
-    {
-      title: "Ant Design Title 4",
-    },
-  ];
+  const data = props.data;
   return (
     <>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta title={item.title} />
-          </List.Item>
-        )}
-      ></List>
+      <PageHeader className="site-page-header" title={props.title}>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta title={item.type} />
+            </List.Item>
+          )}
+        ></List>{" "}
+      </PageHeader>
     </>
   );
 }
@@ -60,13 +67,13 @@ function ScheduleStatistic(props) {
   return (
     <Row gutter={16}>
       <Col span={6}>
-        <Statistic title="当前注册数量" value={2} />
+        <Statistic title="当前注册数量" value={props.initCount} />
       </Col>
       <Col span={6}>
-        <Statistic title="当前运行数量" value={2} />
+        <Statistic title="当前运行数量" value={props.runningCount} />
       </Col>
       <Col span={6}>
-        <Statistic title="共计运行次数" value={200} />
+        <Statistic title="共计运行次数" value={props.totalRuningCount} />
       </Col>
     </Row>
   );
